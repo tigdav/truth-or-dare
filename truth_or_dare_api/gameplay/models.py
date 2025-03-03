bff4be1359f75d6624fbae0ed0c59b211e875df6
tigdav <47061880+tigdav@users.noreply.github.com>
2025-03-03 22:26:56 +0300

    Add verbose_name for fields in QuestionCategory model

--- truth_or_dare_api/gameplay/models.py
+++ truth_or_dare_api/gameplay/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class QuestionCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    is_adult = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
