3f282a4f752f04289f8b183c70ae5d93646e19d8
TigDav <tigdav2004@inbox.ru>
2025-03-03 23:17:58 +0300

    Update Question model and admin panel configuration
    - Added verbose_name for fields in the QuestionCategory and Question models.
    - Added Meta class to both models for better customization in the admin panel.
    - Changed categories field in Question model from ManyToManyField to ForeignKey to restrict selection to only one category per question.

--- truth_or_dare_api/gameplay/models.py
+++ truth_or_dare_api/gameplay/models.py
@@ -2,23 +2,37 @@ from django.db import models
 
 
 class QuestionCategory(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    is_adult = models.BooleanField(default=False)
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Краткое описание')
+    is_adult = models.BooleanField(default=False, verbose_name='Категория для взрослых (18+)')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Категория вопроса'
+        verbose_name_plural = 'Категории вопросов'
+
 
 class Question(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Содержание вопроса')
 
     question_type = models.CharField(
         max_length=100,
-        choices=[('truth', 'Правда'), ('dare', 'Действие')]
+        choices=[('truth', 'Правда'), ('dare', 'Действие')],
+        verbose_name='Тип вопроса'
     )
 
-    categories = models.ManyToManyField(QuestionCategory, related_name='questions')
+    categories = models.ForeignKey(
+        QuestionCategory,
+        on_delete=models.CASCADE,
+        related_name='questions',
+        verbose_name='Категория вопроса'
+    )
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
