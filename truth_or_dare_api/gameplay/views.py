5352d67fcb0d1d919ee53fbd3493f0fb2e5a4842
TigDav <tigdav2004@inbox.ru>
2025-03-08 17:06:35 +0300

    Add routing for Question and QuestionCategory API
    - Created urls.py in the gameplay app for routing API endpoints.
    - Updated the project's main urls.py to include routes from the gameplay app.
    - Added QuestionViewSet and QuestionCategoryViewSet for handling API requests.

--- truth_or_dare_api/gameplay/views.py
+++ truth_or_dare_api/gameplay/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Question, QuestionCategory
+from .serializers import QuestionSerializer, QuestionCategorySerializer
 
-# Create your views here.
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionCategoryViewSet(viewsets.ModelViewSet):
+    queryset = QuestionCategory.objects.all()
+    serializer_class = QuestionCategorySerializer
