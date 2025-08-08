da5d1bac3ba87ce26a4d42409e6c0e32caa55ae2
TigDav <tigdav2004@inbox.ru>
2025-08-08 21:46:24 +0300

    Refactor Question API in gameplay app
    - Move get_random_questions to QuestionViewSet as @action
    - Replace standalone random questions endpoint with DRF router action
    - Clean urls.py by removing redundant endpoint

--- backend/apps/gameplay/views.py
+++ backend/apps/gameplay/views.py
@@ -1,20 +1,31 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from .models import Question, QuestionCategory, Rule
 from .serializers import QuestionSerializer, QuestionRequestSerializer, QuestionCategorySerializer, RuleSerializer
 from django.db.models.functions import Random
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .permissions import IsAdminOrReadOnly
 
 
-@api_view(['POST'])
-def get_random_questions(request):
-    serializer = QuestionRequestSerializer(data=request.data)
-    if serializer.is_valid():
-        question_type = serializer.validated_data['question_type']
-        category_ids = serializer.validated_data['category_ids']
-        excluded_ids = serializer.validated_data.get('excluded_ids', [])
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(
+        detail=False,
+        methods=['post'],
+        url_path='random',
+        permission_classes=[AllowAny]
+    )
+    def get_random_questions(self, request):
+        req_serializer = QuestionRequestSerializer(data=request.data)
+        req_serializer.is_valid(raise_exception=True)
+
+        question_type = req_serializer.validated_data['question_type']
+        category_ids = req_serializer.validated_data['category_ids']
+        excluded_ids = req_serializer.validated_data.get('excluded_ids', [])
 
         queryset = Question.objects.filter(
             question_type=question_type,
@@ -23,16 +34,8 @@ def get_random_questions(request):
 
         random_questions = queryset.order_by(Random())[:5]
 
-        serialized = QuestionSerializer(random_questions, many=True)
-        return Response(serialized.data)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class QuestionViewSet(viewsets.ModelViewSet):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    permission_classes = [IsAdminUser]
+        serializer = QuestionSerializer(random_questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class QuestionCategoryViewSet(viewsets.ModelViewSet):
